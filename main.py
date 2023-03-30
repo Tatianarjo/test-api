8e12e653c31bb988d494033f415ae88cb37f441b
Tatiana Johnson <Tatianarjo@gmail.com>
2023-03-30 14:31:32 -0500

    created database

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
-import random
+from fastapi import FastAPI, Path
+from uuid import uuid4
+from typing import List
+from models import User, Role, Gender
 
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(), 
+        first_name= "Peaches",
+        last_name="Baldeaches",
+        date_of_birth="October 17, 1934",
+        gender=Gender.female,
+        roles=[Role.user]
+        ),
+    User(
+        id=uuid4(), 
+        first_name= "George",
+        last_name="Pineapple",
+        date_of_birth="March 17, 2023",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+        )
+]
 
 #@app.get("/reports")
 #def reports():
@@ -11,15 +32,23 @@ app = FastAPI()
 #def labreports():
    # return{}
 
-lab_Report = {
+"""patient = {
     1: {
-        "First_Name": "Peaches",
-        "Last_Name": "Deaches",
+        "name": "Peaches",
+        "lastName": "Deaches",
         "Date of Birth": "April 17, 2023",
         "Gender":"Female"
     }
-}
+},"""
 
-@app.get("/get-report/{report_id}/{name}")
-def get_report(report_id: int, name: str):
-    return lab_Report[report_id]
+
+"""@app.get("/get-patient/{patient_id}")
+def get_patient(patient_id: int = Path(description="The ID of the patient you would like to view")):
+    return patient[patient_id]"""
+
+"""@app.get("/get-by-name")
+def get_patient(name: str):
+    for patient_id in patient:
+        if patient[patient_id]["name"] == name:
+            return patient[patient_id]
+        return{"Data": "Not Found"}"""
