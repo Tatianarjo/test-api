9e0bad869c23393b735c3f2d00941828b4034237
Tatiana Johnson <Tatianarjo@gmail.com>
2023-03-29 21:04:27 -0500

    url takes two parameters

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import random
 
 app = FastAPI()
 
-@app.get("/reports")
-def reports():
-    return{"Data": "Test"}
+#@app.get("/reports")
+#def reports():
+  #  return{"Data": "Test"}
+
+#@app.get("/labreports")
+#def labreports():
+   # return{}
+
+lab_Report = {
+    1: {
+        "First_Name": "Peaches",
+        "Last_Name": "Deaches",
+        "Date of Birth": "April 17, 2023",
+        "Gender":"Female"
+    }
+}
+
+@app.get("/get-report/{report_id}/{name}")
+def get_report(report_id: int, name: str):
+    return lab_Report[report_id]
