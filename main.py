b954be08e610176cd234732efe06c64480f8a169
Tatiana Johnson <Tatianarjo@gmail.com>
2023-04-03 12:45:22 -0500

    added GET method to get patients by datetime

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from uuid import UUID
 from typing import List, Optional
+from datetime import datetime
 from models import User, Role, Gender, UserUpdateRequest, UserResponse
 
 app = FastAPI()
 
-"""db: List[User] = [
-    User(
-        id=UUID("d18d6cf9-3d79-43cb-87a9-17b5d247d764"), 
-        first_name= "Peaches",
-        last_name="Baldeaches",
-        date_of_birth="October 17, 1934",
-        gender=Gender.female,
-        roles=[Role.user],
-        type_of_test= "Glucose",
-        result= "Postive",
-        time_of_test= "3PM"
-        ),
-    User(
-        id=UUID("2f6a369a-ec64-4319-9f5b-ab0e0ab0b0f3"), 
-        first_name= "George",
-        last_name="Pineapple",
-        date_of_birth="March 17, 2023",
-        gender=Gender.male,
-        roles=[Role.admin, Role.user],
-        type_of_test="Blood Test",
-        result= "Negative",
-        time_of_test= "4PM"
-        ),
-    
-],"""
 
 db: List[User] = [
     User(
@@ -69,15 +45,17 @@ db: List[User] = [
 ]
 
 
-
+##The opening page
 @app.get("/")
 async def root():
     return {"Welcome to the": "Patient Portal"}
 
+##This shows all patients
 @app.get("/api/v1/users")
 async def fetch_users():
     return db;
 
+##This shows all patients by the test type
 @app.get("/api/v1/users/test{test_type}")
 async def get_user_by_test(test_type: str):
     users_info = []
@@ -99,13 +77,13 @@ async def get_user_by_test(test_type: str):
         raise HTTPException(status_code=404, detail=f"No users found with test type: {test_type}")
     return users_info
             
-
+##This is how you would register a patient
 @app.post("/api/v1/users")
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
 
-
+##Here you can delete the patient
 @app.delete("/api/v1/users/{user_id}")
 async def delete_user(user_id: UUID):
     for user in db:
@@ -135,6 +113,7 @@ async def update_user(user_update: UserUpdateRequest, user_id: UUID):
             detail=f"user with id: {user_id} does not exist"
         )"""
 
+##Here is where you can update a patient's information
 @app.put("/api/v1/users/{user_id}", response_model=UserResponse)
 async def update_user(user_id: UUID, user_update: UserUpdateRequest):
     try:
@@ -164,12 +143,33 @@ async def update_user(user_id: UUID, user_update: UserUpdateRequest):
 
     return UserResponse(**user.dict())
 
+"""@app.get("/api/v1/users/test/{test_type}")
+async def get_user_by_test(test_type: str, start_date: str, end_date: str):
+    users_info = []
+    for user in db:
+        if user.type_of_test == test_type:
+            user_time_of_test = datetime.strptime(user.time_of_test, '%I%p')
+            start = datetime.strptime(start_date, '%Y-%m-%d %I%p')
+            end = datetime.strptime(end_date, '%Y-%m-%d %I%p')
+            if start <= user_time_of_test <= end:
+                user_info = {
+                    "id": user.id,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "date_of_birth": user.date_of_birth,
+                    "gender": user.gender,
+                    "roles": user.roles,
+                    "type_of_test": user.type_of_test,
+                    "result": user.result,
+                    "time_of_test": user.time_of_test
+                }
+                users_info.append(user_info)
+    if not users_info:
+        raise HTTPException(status_code=404, detail=f"No users found with test type: {test_type}")
+    return users_info"""
         
-
+# show all patients that have had a certain type of lab report in a datetime range.
      
-
-#here is where I return the entire database with the addition of a new user and their id
-
 
 
 
